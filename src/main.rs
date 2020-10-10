bd1ab019d80f2bb9f9e75ce2bf688a32f221f7cd
Martichou <martichou.andre@gmail.com>
2020-10-10 22:34:39 +0200

    Functionify and handle Command possible failure

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::process::Command;
 
 use sys_info::hostname;
 
-use sysinfo::{ProcessorExt, System, SystemExt};
 use sysinfo::ComponentExt;
+use sysinfo::{ProcessorExt, System, SystemExt};
 
+/* Get uptime of the computer in a special format */
 fn get_uptime(sys: &System) -> String {
     let mut uptime = sys.get_uptime();
     let days = uptime / 86400;
@@ -18,23 +19,54 @@ fn get_uptime(sys: &System) -> String {
     return format!("{} days {} hours {} minutes", days, hours, minutes);
 }
 
+/* Retrieve the MAC address using the prefix as target [78: for 19's iMac] */
+fn get_mac_address(prefix: String) -> String {
+    let mac_address = Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "ifconfig | grep {} | awk {} | tr -d '\n'",
+            prefix, "'{print $2}'"
+        ))
+        .output();
+    return match mac_address {
+        Ok(val) => String::from_utf8_lossy(&val.stdout).to_string(),
+        Err(x) => x.to_string(),
+    };
+}
+
+/* Get the user currently logged, if more than 1 user, return the last one */
+fn get_logged_user() -> String {
+    let logged_users = Command::new("bash")
+        .arg("-c")
+        .arg("users | awk -F' ' '{print $NF}' | tr -d '\n'")
+        .output();
+    return match logged_users {
+        Ok(val) => String::from_utf8_lossy(&val.stdout).to_string(),
+        Err(x) => x.to_string(),
+    };
+}
+
 fn main() {
     let sys = System::new_all();
 
-    let hostname = hostname().unwrap();
     let os_release = os_version::detect().unwrap();
-
-    let uuid = machine_uid::get().unwrap();
-
     println!("os: {}", os_release.to_string());
+
+    let hostname = hostname().unwrap();
     println!("hostname: {}", hostname);
-    println!("{}", get_uptime(&sys));
+    
+    let uptime = get_uptime(&sys);
+    println!("{}", uptime);
+    
+    let uuid = machine_uid::get().unwrap();
     println!("uuid {}", uuid);
+    
     println!(
         "Memory: {}/{} MB",
         sys.get_used_memory() / 1000,
         sys.get_total_memory() / 1000
     );
+
     println!(
         "CPU : {} MHz, {} %",
         sys.get_processors()[0].get_frequency(),
@@ -42,20 +74,15 @@ fn main() {
     );
 
     for component in sys.get_components() {
-        println!("{}: {}°C", component.get_label(), component.get_temperature());
+        println!(
+            "{}: {}°C",
+            component.get_label(),
+            component.get_temperature()
+        );
     }
-    let logged_users = Command::new("bash")
-        .arg("-c")
-        .arg("users | awk -F' ' '{print $NF}' | tr -d '\n'")
-        .output()
-        .expect("failed to execute process");
-    println!("Users: {}", String::from_utf8_lossy(&logged_users.stdout));
-
-    // Get the mac address - only valid for the iMac at 19 as they start by 78:
-    let mac_address = Command::new("bash")
-        .arg("-c")
-        .arg("ifconfig | grep 78: | awk -F' ' '{print $NF}' | tr -d '\n'")
-        .output()
-        .expect("failed to execute process");
-    println!("MAC: {}", String::from_utf8_lossy(&mac_address.stdout));
+    let logged_users = get_logged_user();
+    println!("Users: {}", logged_users);
+    
+    let mac_address = get_mac_address("48:".to_string());
+    println!("MAC: {}", mac_address);
 }

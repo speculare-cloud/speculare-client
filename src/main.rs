8dd9d8f46d9dae3f5c625421c4712e2a7d3f4015
Martichou <martichou.andre@gmail.com>
2020-10-11 12:12:46 +0200

    get the MAC Address of the default interface

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,54 @@ mod models;
 
 use crate::models::*;
 
+use std::error::Error;
 use std::process::Command;
 use sys_info::hostname;
-use std::error::Error;
 use sysinfo::ComponentExt;
 use sysinfo::{ProcessorExt, System, SystemExt};
 
-/* Retrieve the MAC address using the prefix as target [78: for 19's iMac] */
-fn get_mac_address(prefix: String) -> String {
+/*
+ *  MAC - linux specific get_mac_address
+ *  using default interface in a safe string
+ */
+#[cfg(target_os = "linux")]
+fn get_mac_address() -> String {
+    let interface = Command::new("bash")
+        .arg("-c")
+        .arg("route | grep '^default' | grep -o '[^ ]*$' | sed '$!d' | tr -d '\n'")
+        .output()
+        .expect("failed to retrieve default interface");
     let mac_address = Command::new("bash")
         .arg("-c")
         .arg(format!(
-            "ifconfig | grep {} | awk {} | tr -d '\n'",
-            prefix, "'{print $2}'"
+            "ifconfig {} | grep 'ether ' | awk {} | tr -d '\n'",
+            String::from_utf8_lossy(&interface.stdout),
+            "'{print $2}'"
+        ))
+        .output();
+    return match mac_address {
+        Ok(val) => String::from_utf8_lossy(&val.stdout).to_string(),
+        Err(x) => x.to_string(),
+    };
+}
+
+/*
+ *  MAC - macos specific get_mac_address
+ *  using default interface in a safe string
+ */
+#[cfg(target_os = "macos")]
+fn get_mac_address() -> String {
+    let interface = Command::new("bash")
+        .arg("-c")
+        .arg("route -n get default | grep 'interface:' | grep -o '[^ ]*$' | sed '$!d' | tr -d '\n'")
+        .output()
+        .expect("failed to retrieve default interface");
+    let mac_address = Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "ifconfig {} | grep 'ether ' | awk {} | tr -d '\n'",
+            String::from_utf8_lossy(&interface.stdout),
+            "'{print $2}'"
         ))
         .output();
     return match mac_address {
@@ -43,24 +78,24 @@ fn get_os_version() -> String {
     let os_release = os_version::detect();
     return match os_release {
         Ok(val) => val.to_string(),
-        Err(x) => x.to_string()
-    }
+        Err(x) => x.to_string(),
+    };
 }
 
 /* Get the hostname (Mac/Linux/Windows) in a safe String */
 fn get_hostname() -> String {
     return match hostname() {
         Ok(val) => val.to_string(),
-        Err(x) => x.to_string()
-    }
+        Err(x) => x.to_string(),
+    };
 }
 
 /* Get the uuid of the host (Mac/Linux/Windows) in a safe String */
 fn get_uuid() -> String {
     return match machine_uid::get() {
         Ok(val) => val.to_string(),
-        Err(x) => x.to_string()
-    }
+        Err(x) => x.to_string(),
+    };
 }
 
 #[tokio::main]
@@ -75,7 +110,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         })
     }
 
-    // TODO - Replace the 48: in a env file
     let data = Data {
         os: get_os_version(),
         hostname: get_hostname(),
@@ -84,13 +118,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         cpu_freq: sys.get_processors()[0].get_frequency(),
         user: get_logged_user(),
         sensors: sensors,
-        mac_address: get_mac_address("48:".to_string()),
+        mac_address: get_mac_address(),
     };
 
     let client = reqwest::Client::new();
     // TODO - Change the URL in a env file
     let res = client
-        .post("https://enmy1ryyhupko.x.pipedream.net")
+        .post("https://enj6lyfuy1r7g.x.pipedream.net")
         .json(&data)
         .send()
         .await?;

49df26fc0e55dc4fd4b4df885e76ef9b8c010fb5
Martichou <martichou.andre@gmail.com>
2020-11-14 17:55:06 +0100

    Noticed that getting ONE MAC address is useless, removing until proper implementation

--- benches/network_bench.rs
+++ benches/network_bench.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main};
 use metrics_rs::network::*;
 
 pub fn network_benches(c: &mut Criterion) {
-    c.bench_function("get_mac_address", |b| b.iter(|| get_mac_address()));
+    
 }
 
 criterion_group!(benches, network_benches);

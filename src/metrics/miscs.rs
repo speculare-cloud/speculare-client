13ca847e3aa52437b5b8c2c7cba32b1225c630a1
Martichou <martichou.andre@gmail.com>
2020-11-17 23:16:35 +0100

    Fixing get_host_info - need to replace the Memory for macOS

--- src/metrics/miscs.rs
+++ src/metrics/miscs.rs
@@ -3,10 +3,6 @@ use super::read_and_trim;
 
 use crate::models;
 
-use models::{HostInfo, LoadAvg, Memory};
-use nix::sys;
-use std::io::{Error, ErrorKind};
-
 #[cfg(target_os = "macos")]
 use core_foundation_sys::{
     base::CFTypeRef,
@@ -16,8 +12,17 @@ use core_foundation_sys::{
 use io_kit_sys::*;
 #[cfg(target_os = "macos")]
 use libc::c_char;
+#[cfg(target_family = "unix")]
+use libc::{c_double, getloadavg};
+#[cfg(target_os = "macos")]
+use libc::{c_void, sysctl, timeval};
+use models::{HostInfo, LoadAvg, Memory};
+use nix::sys;
 #[cfg(target_os = "macos")]
 use std::ffi::{CStr, CString};
+use std::io::{Error, ErrorKind};
+#[cfg(target_os = "macos")]
+use std::time::Duration;
 
 /// Get the os version (Mac/Linux/Windows) in a safe String.
 /// Take approx 0,080ms to load the info 'os_info::get()'.
@@ -33,27 +38,90 @@ pub fn get_hostname() -> String {
     x.nodename().to_owned()
 }
 
+#[cfg(target_os = "macos")]
+pub fn get_uptime() -> Result<Duration, Error> {
+    let mut data: timeval = unsafe { std::mem::zeroed() };
+    let mib = [1, 21];
+    let ret = unsafe {
+        sysctl(
+            &mib[0] as *const _ as *mut _,
+            mib.len() as u32,
+            &mut data as *mut _ as *mut c_void,
+            &mut std::mem::size_of::<timeval>(),
+            std::ptr::null_mut(),
+            0,
+        )
+    };
+    if ret < 0 {
+        Err(Error::new(ErrorKind::Other, "Invalid return for sysctl"))
+    } else {
+        Ok(Duration::from_secs(data.tv_sec as u64))
+    }
+}
+
+#[cfg(target_family = "unix")]
+pub fn get_loadavg() -> Result<LoadAvg, Error> {
+    let mut data: [c_double; 3] = [0.0, 0.0, 0.0];
+    if unsafe { getloadavg(data.as_mut_ptr(), 3) } == -1 {
+        return Err(Error::new(
+            ErrorKind::Other,
+            "Invalid return for getloadavg",
+        ));
+    }
+    Ok(LoadAvg {
+        one: data[0],
+        five: data[1],
+        fifteen: data[2],
+    })
+}
+
 /// Get both hostname and os_version from the same single uname instance.
+#[cfg(target_os = "linux")]
 pub fn get_host_info() -> Result<HostInfo, Error> {
     let x = sys::utsname::uname();
     let y = match sys::sysinfo::sysinfo() {
         Ok(val) => val,
         Err(x) => return Err(Error::new(ErrorKind::Other, x)),
     };
-    let loadavg = y.load_average();
+    let uptime = y.uptime().as_secs() as i64;
+    let loadavg_raw = y.load_average();
+    let loadavg = LoadAvg {
+        one: loadavg_raw.0,
+        five: loadavg_raw.1,
+        fifteen: loadavg_raw.2,
+    };
+    let memory = Memory {
+        total_virt: y.ram_total() as i64,
+        total_swap: y.swap_total() as i64,
+        avail_virt: y.ram_unused() as i64,
+        avail_swap: y.swap_free() as i64,
+    };
     Ok(HostInfo {
-        loadavg: LoadAvg {
-            one: loadavg.0,
-            five: loadavg.1,
-            fifteen: loadavg.2,
-        },
-        memory: Memory {
-            total_virt: y.ram_total() as i64,
-            total_swap: y.swap_total() as i64,
-            avail_virt: y.ram_unused() as i64,
-            avail_swap: y.swap_free() as i64,
-        },
-        uptime: y.uptime().as_secs() as i64,
+        loadavg: loadavg,
+        memory: memory,
+        uptime: uptime,
+        os_version: x.sysname().to_owned() + "/" + x.release(),
+        hostname: x.nodename().to_owned(),
+    })
+}
+
+/// Get both hostname and os_version from the same single uname instance.
+#[cfg(target_os = "macos")]
+pub fn get_host_info() -> Result<HostInfo, Error> {
+    let x = sys::utsname::uname();
+    let uptime = get_uptime().unwrap().as_secs() as i64;
+    let loadavg = get_loadavg().unwrap();
+    // TODO
+    let memory = Memory {
+        total_virt: 0,
+        total_swap: 0,
+        avail_virt: 0,
+        avail_swap: 0,
+    };
+    Ok(HostInfo {
+        loadavg: loadavg,
+        memory: memory,
+        uptime: uptime,
         os_version: x.sysname().to_owned() + "/" + x.release(),
         hostname: x.nodename().to_owned(),
     })
@@ -68,6 +136,8 @@ pub fn get_uuid() -> Result<String, Error> {
         Err(_) => Ok(read_and_trim("/var/lib/dbus/machine-id")?),
     }
 }
+/// Get the machine UUID (macOS) as a String.
+/// macOS => Will get it from some black magic extern C function.
 #[cfg(target_os = "macos")]
 pub fn get_uuid() -> Result<String, Error> {
     let serial_number;

0635a1ed25eb90d883f4ac23da4dc504696b64b1
Martichou <m@rtin.fyi>
2022-09-13 19:53:48 +0200

    deps: routine update/upgrade

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -8,7 +8,7 @@ pub fn cget_uuid() -> String {
     // as per https://man7.org/linux/man-pages/man5/machine-id.5.html
     // the machine-id should never be shared on the network or other.
     match get_uuid() {
-        Ok(uuid) => sha1_smol::Sha1::from(&uuid).hexdigest(),
+        Ok(uuid) => sha1_smol::Sha1::from(uuid).hexdigest(),
         Err(_) => {
             let host_info = match get_host_info() {
                 Ok(info) => info,
@@ -17,7 +17,7 @@ pub fn cget_uuid() -> String {
                     std::process::exit(1);
                 }
             };
-            sha1_smol::Sha1::from(&host_info.hostname).hexdigest()
+            sha1_smol::Sha1::from(host_info.hostname).hexdigest()
         }
     }
 }
